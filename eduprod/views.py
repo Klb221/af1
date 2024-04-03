b23144746c99fac35f06f4ebda9847fb1f983d38
Klb221 <Kian.Longley-Bain@education.nsw.gov.au>
2024-04-03 14:44:36 +1100

    Debug restart | Klb221

--- eduprod/views.py
+++ eduprod/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request):
-    questions = Question.objects.all()
+    questions = Question.objects.order_by('?')[:2]
     questions_json = serializers.serialize('json', questions)
     return render(request, 'eduprod/index.html', {'questions_json': questions_json})
