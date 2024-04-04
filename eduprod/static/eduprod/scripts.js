ee132a6a7f1530bca642e06b3ed302c81063da26
Klb221 <Kian.Longley-Bain@education.nsw.gov.au>
2024-04-04 21:48:56 +1100

    Final commit :( | Klb221

--- eduprod/static/eduprod/scripts.js
+++ eduprod/static/eduprod/scripts.js
@@ -2,30 +2,40 @@ document.addEventListener("DOMContentLoaded", function() {
     let currentQuestionIndex = 0;
     const questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
     const content = document.getElementById('content');
-    const btn = document.getElementById('revealBtn');
+    const true_btn = document.getElementById('trueBtn');
+    const false_btn = document.getElementById('falseBtn');
+    const reset_btn = document.getElementById('resetBtn');
+    const result = document.getElementById('outputTxt');
 
     function displayQuestion() {
         if (currentQuestionIndex < questions.length) {
             const question = questions[currentQuestionIndex].fields.question_text;
             const answer = questions[currentQuestionIndex].fields.answer_text;
             content.innerHTML = `<div class='question'>Question: ${question}</div><div class='answer' style='display: none;'>Answer: ${answer}</div>`;
-            btn.textContent = "Reveal Answer";
+            true_btn.textContent = "True";
+            false_btn.textContent = "False";
+            reset_btn.textContent = "Reset";
+            result.style.display = "none";
+
         } else {
             content.innerHTML = "No more questions.";
-            btn.style.display = "none";
+            true_btn.style.display = "none";
+            false_btn.style.display = "none";
+            reset_btn.style.display = "none";
+            result.style.display = "none";
         }
     }
 
-    displayQuestion();
-
-    btn.addEventListener("click", function() {
+    true_btn.addEventListener("click", function() {
         const answerElement = content.querySelector('.answer');
-        if (btn.textContent === "Reveal Answer") {
-            answerElement.style.display = "block";
-            btn.textContent = "Next Question";
+        if (answerElement === "True") {
+            true_btn.style.display = "none";
+            false_btn.style.display = "none";
+            reset_btn.style.display = "none";
+            result.style.display = "bloak";
         } else {
-            currentQuestionIndex++;
-            displayQuestion();
+            // Do stuff here
         }
+        // replicate section for the 3 buttons
     });
 });
